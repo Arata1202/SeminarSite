9b177496272f1609b908115e05161f64b36e65b2
Arata <aratadayoon@yahoo.co.jp>
2024-12-31 13:48:36 +0900

    refactor:

--- components/Elements/SearchField/index.tsx
+++ components/Elements/SearchField/index.tsx
@@ -7,7 +7,7 @@ export default function SearchField({ defaultQuery = '' }) {
       <input
         type="search"
         name="q"
-        className={`${styles.search} hover:border-blue-500`}
+        className={`${styles.search} border border-gray-300 hover:border-blue-500`}
         placeholder="検索"
         defaultValue={defaultQuery}
         style={{ width: '100%', borderRadius: '0' }}
