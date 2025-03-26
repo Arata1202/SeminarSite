3dd2ac05dce7b93b04b7952353229ba0e426c99a
Arata <aratadayoon@yahoo.co.jp>
2025-03-26 17:10:14 +0900

    add: sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,53 @@
+import type { MetadataRoute } from 'next';
+import { getAllLists } from '@/libs/microcms';
+import { CATEGORY_ARR } from '@/constants/category';
+import { PAGE_ARR } from '@/constants/page';
+
+export const dynamic = 'force-static';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const articles = await getAllLists();
+
+  const url = process.env.NEXT_PUBLIC_BASE_URL;
+  const lastModified = new Date();
+  const changeFrequency = 'daily';
+  const priority = 0.7;
+
+  const topPage: MetadataRoute.Sitemap = [
+    {
+      url: `${url}`,
+      lastModified: lastModified,
+      changeFrequency: changeFrequency,
+      priority: priority,
+    },
+  ];
+
+  const appPages: MetadataRoute.Sitemap = PAGE_ARR.map((page) => {
+    return {
+      url: `${url}/${page.path}`,
+      lastModified: lastModified,
+      changeFrequency: changeFrequency,
+      priority: priority,
+    };
+  });
+
+  const categoryPages: MetadataRoute.Sitemap = CATEGORY_ARR.map((category) => {
+    return {
+      url: `${url}/blog/category/${category.id}`,
+      lastModified: lastModified,
+      changeFrequency: changeFrequency,
+      priority: priority,
+    };
+  });
+
+  const articlePages: MetadataRoute.Sitemap = articles.map((article) => {
+    return {
+      url: `${url}/blog/articles/${article.id}`,
+      lastModified: article.updatedAt,
+      changeFrequency: changeFrequency,
+      priority: priority,
+    };
+  });
+
+  return [...topPage, ...appPages, ...categoryPages, ...articlePages];
+}
---
+++ src/constants/page.ts
@@ -0,0 +1,11 @@
+export const PAGE_ARR = [
+  { name: '活動内容', path: 'activity' },
+  { name: 'お問い合わせ', path: 'contact' },
+  { name: '著作権', path: 'copyright' },
+  { name: '免責事項', path: 'disclaimer' },
+  { name: 'リンク', path: 'link' },
+  { name: '組織', path: 'member' },
+  { name: 'プライバシーポリシー', path: 'privacy' },
+  { name: '選考', path: 'selection' },
+  { name: 'ブログ', path: 'blog' },
+];
