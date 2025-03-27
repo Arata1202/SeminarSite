98687874c61fca9b4627cf2538bec22b128214bd
Arata <aratadayoon@yahoo.co.jp>
2025-03-27 22:08:44 +0900

    refactor: ArticleList

--- src/components/Common/ArticleList/index.tsx
+++ src/components/Common/ArticleList/index.tsx
@@ -3,51 +3,31 @@ import ArticleCard from '../ArticleCard';
 import Sidebar from '../Layouts/Sidebar';
 
 type Props = {
-  articles?: Article[];
+  articles: Article[];
 };
 
 export default function ArticleList({ articles }: Props) {
-  if (!articles) {
-    return null;
-  }
-  if (articles.length === 0) {
-    return (
-      <div className="mx-auto max-w-7xl px-6 lg:px-8 main_side">
-        <div className="mx-auto text-base leading-7">
-          {/* <p className="text-base font-semibold leading-7 text-indigo-600">Introducing</p> */}
-          <div className="max-w-[87rem] mx-auto">
-            <div className="grid lg:grid-cols-3 gap-y-8 lg:gap-y-0 lg:gap-x-6">
-              <div className="lg:col-span-2">
-                <div className="text-center pt-7">
-                  <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
-                    記事はまだありません
-                  </h1>
-                </div>
-              </div>
-              <Sidebar />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
   return (
     <>
       <div className="mx-auto max-w-7xl px-6 lg:px-8 main_side">
-        <div className="mx-auto text-base leading-7">
-          {/* <p className="text-base font-semibold leading-7 text-indigo-600">Introducing</p> */}
-          <div className="max-w-[87rem] mx-auto">
-            <div className="grid lg:grid-cols-3 gap-y-8 lg:gap-y-0 lg:gap-x-6">
-              <div className="lg:col-span-2">
-                <ul>
-                  {articles.map((article) => (
-                    <ArticleCard key={article.id} article={article} />
-                  ))}
-                </ul>
+        <div className="grid lg:grid-cols-3 gap-y-8 lg:gap-y-0 lg:gap-x-6">
+          <div className="lg:col-span-2">
+            {articles.length === 0 && (
+              <div className="text-center pt-7">
+                <div className="mt-4 text-5xl font-bold tracking-tight sm:text-5xl">
+                  記事はまだありません
+                </div>
               </div>
-              <Sidebar />
-            </div>
+            )}
+            {articles.length > 0 && (
+              <ul>
+                {articles.map((article) => (
+                  <ArticleCard key={article.id} article={article} />
+                ))}
+              </ul>
+            )}
           </div>
+          <Sidebar />
         </div>
       </div>
     </>
