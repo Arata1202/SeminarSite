1e87eafc38e25f67a3c8775ac679b31411b27fe8
Arata <aratadayoon@yahoo.co.jp>
2025-03-29 12:11:02 +0900

    refactor: css

--- src/components/Features/Activity/Elements/Schedule/index.tsx
+++ src/components/Features/Activity/Elements/Schedule/index.tsx
@@ -1,3 +1,4 @@
+import styles from './index.module.css';
 import PageTitle from '@/components/Common/PageTitle';
 
 type Props = {
@@ -9,8 +10,16 @@ export default function Schedule({ title, Icon }: Props) {
   return (
     <div className="mx-auto max-w-7xl">
       <PageTitle title={title} Icon={Icon} />
-      <img className="pc mt-10" src="/images/activity/pc.png" alt="" />
-      <img className="mobile mt-10" src="/images/activity/mobile.png" alt="" />
+      <img
+        className={`${styles.pc} block mt-10`}
+        src="/images/activity/pc.png"
+        alt="スケジュール"
+      />
+      <img
+        className={`${styles.mobile} block mt-10`}
+        src="/images/activity/mobile.png"
+        alt="スケジュール"
+      />
     </div>
   );
 }
