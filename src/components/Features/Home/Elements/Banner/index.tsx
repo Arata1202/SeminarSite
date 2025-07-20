704cebedba1b141882451a374435233420a2a464
Arata <aratadayoon@yahoo.co.jp>
2025-07-20 15:53:59 +0900

    Remove ScrollAnimation wrapper from Banner component

--- src/components/Features/Home/Elements/Banner/index.tsx
+++ src/components/Features/Home/Elements/Banner/index.tsx
@@ -1,5 +1,4 @@
 import styles from './index.module.css';
-import ScrollAnimation from '@/components/Common/Animation/ScrollAnimation';
 
 type Props = {
   title: string;
@@ -7,12 +6,10 @@ type Props = {
 
 export default function Banner({ title }: Props) {
   return (
-    <ScrollAnimation variant="slideInFromBottom" duration={1.0}>
-      <div
-        className={`${styles.container} flex justify-center gap-x-1 bg-green-700 px-6 py-2.5 sm:px-3.5`}
-      >
-        <p className="text:sm sm:text-2xl font-bold leading-6 text-white">{title}</p>
-      </div>
-    </ScrollAnimation>
+    <div
+      className={`${styles.container} flex justify-center gap-x-1 bg-green-700 px-6 py-2.5 sm:px-3.5`}
+    >
+      <p className="text:sm sm:text-2xl font-bold leading-6 text-white">{title}</p>
+    </div>
   );
 }
